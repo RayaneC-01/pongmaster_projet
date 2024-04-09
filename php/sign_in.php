4166ac5ea51ed9731c87ca3e92933f0863e1da1c
Rayane <rchaabane20@gmail.com>
2024-04-09 05:07:00 +0200

    Modifications dans sign_in.php

--- php/sign_in.php
+++ php/sign_in.php
@@ -17,6 +17,7 @@ session_start();
 </head>
 
 <body>
+
     <div class="container">
         <form class="login-form" method="POST" action="/php/sign_in_processing.php">
             <h2>Connexion</h2>
@@ -43,7 +44,7 @@ session_start();
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-        </script>
+    </script>
     <script src="/Script/script.js"></script>
 </body>
 

be64e76aa5470d637a8817f0d7fc4c98191f9015
Rayane <rchaabane20@gmail.com>
2024-05-22 01:31:06 +0200

    Modification du CSS

--- php/sign_up.php
+++ php/sign_up.php
@@ -14,7 +14,7 @@ session_start();
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <!-- Bootstrap icons (optional) -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="/styles/style.css" />
+    <link rel="stylesheet" href="../CSS/style.css">
 </head>
 
 <body>
@@ -58,7 +58,7 @@ session_start();
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+        </script>
     <script src="/Script/script.js"></script>
 </body>
 
